8eff01543fdd1b4b332cd36048dc2fc30a7eb275
Burr Sutter <burrsutter@gmail.com>
2024-09-18 05:51:39 -0400

    first commit

---
+++ src/main/java/com/burrsutter/RESTResource.java
@@ -0,0 +1,44 @@
+package com.burrsutter;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class RESTResource {
+
+    @Inject AIGreetingService aigreeter;
+    @Inject AIJSONService aijsongreeter;
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/helloai")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloai() {
+        return aigreeter.greet("Burr");
+    }
+
+    @GET
+    @Path("/helloaijson")
+    @Produces(MediaType.TEXT_PLAIN)
+    public JSONResponse helloaijson() {
+        return aijsongreeter.greet("Burr");
+    }
+
+
+    @GET
+    @Path("/bye")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String bye() {
+        return "Aloha";
+    }
+
+}
